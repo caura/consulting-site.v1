91bccdbc52bda54825910e8a414f10860b4755a9
Segah Meer <segahm@gmail.com>
2018-12-05 11:46:52 -0800

    trim the list of vendors

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,13 +105,13 @@ $(function() {
   | them in this file.
   |
   */
- function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
+   function getUrlVars() {
+      var vars = {};
+      var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+          vars[key] = value;
+      });
+      return vars;
+  }
   function getUrlParam(parameter, defaultvalue){
       var urlparameter = defaultvalue;
       if(window.location.href.indexOf(parameter) > -1){
