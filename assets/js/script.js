6cfd89d05ba5c84cc29277ae8122b522549b497b
Segah Meer <segahm@gmail.com>
2019-01-07 09:47:32 -0800

    make adroll variables global

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,18 +166,26 @@ $(function() {
     /* custom configuration goes here (www.olark.com/documentation) */
     olark.identify(page.defaults.olark);
   }
+
   if (getUrlParam("utm_campaign",null) != 'ignore' && page.defaults.adroll_adv_id && page.defaults.adroll_pix_id){
-    var adroll_adv_id = page.defaults.adroll_adv_id;
-    var adroll_pix_id = page.defaults.adroll_pix_id;
+    window.adroll_adv_id = page.defaults.adroll_adv_id;
+    window.adroll_pix_id = page.defaults.adroll_pix_id;
     (function () {
-        var scr = document.createElement("script");
-        var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-        scr.setAttribute('async', 'true');
-        scr.type = "text/javascript";
-        scr.src = host + "/j/roundtrip.js";
-        ((document.getElementsByTagName('head') || [null])[0] ||
-            document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+        var _onload = function(){
+            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+            var scr = document.createElement("script");
+            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+            scr.setAttribute('async', 'true');
+            scr.type = "text/javascript";
+            scr.src = host + "/j/roundtrip.js";
+            ((document.getElementsByTagName('head') || [null])[0] ||
+                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+        };
+        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+        else {window.attachEvent('onload', _onload)}
     }());
+
     $( document ).ready(function() {
       $( '#chat-start1' ).on("mousedown",
         function(){
